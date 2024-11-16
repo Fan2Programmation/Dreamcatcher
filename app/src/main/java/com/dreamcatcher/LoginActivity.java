849168ce425615bd89ee0ba935a1e7dc3db971e1
Thibault <thibault.terrie.cyu@gmail.com>
2024-11-16 16:17:11 +0100

    Login/Register page working nice with new java API (see Fan2Programmation/interface)

--- app/src/main/java/com/dreamcatcher/LoginActivity.java
+++ app/src/main/java/com/dreamcatcher/LoginActivity.java
@@ -8,6 +8,8 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
+import java.net.SocketTimeoutException;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -16,9 +18,11 @@ import okhttp3.RequestBody;
 
 public class LoginActivity extends AppCompatActivity {
 
-    // 10.0.2.2 is a build-in ip address inside the emulator that represents the device that
-    // is running the emulator (hence my own computer, where my http server is running)
-    private static final String BASE_URL = "http://10.0.2.2:3000";
+    // the address of our spring boot api (enables us to communicate with our database by
+    // using an HTTP interface because psql isn't supported directly with android development)
+    // 10.0.2.2 is the address of the computer on which the simulation is running (hence my
+    // own computer where the api is running)
+    private static final String BASE_URL = "http://10.0.2.2:8080/passerelle/v1/users";
     EditText usernameInput;
     EditText passwordInput;
 
@@ -43,7 +47,7 @@ public class LoginActivity extends AppCompatActivity {
         if (username.isEmpty() || password.isEmpty()) {
             Toast.makeText(this, "Please fill in the blanks", Toast.LENGTH_SHORT).show();
         } else {
-            if(type.equals("login")) loginUser(username, password);
+            if (type.equals("login")) loginUser(username, password);
             else if (type.equals("register")) registerUser(username, password);
         }
     }
@@ -53,31 +57,32 @@ public class LoginActivity extends AppCompatActivity {
             try {
                 OkHttpClient client = new OkHttpClient();
 
-                Request request = new Request.Builder().url(BASE_URL + "/users").build();
+                // this is the json we send to the api
+                String json = "{\"username\": \"" + username + "\", \"password\": \"" + password + "\"}";
+                RequestBody body = RequestBody.create(json, MediaType.parse("application/json"));
 
+                // creating the request
+                Request request = new Request.Builder()
+                        .url(BASE_URL + "/login")
+                        .post(body)
+                        .build();
+
+                // executing the request
                 Response response = client.newCall(request).execute();
 
                 if (response.isSuccessful()) {
-                    String responseData = "";
-                    if(response.body() != null) {
-                        // .string() enables us to get the body of the HTTP answer
-                        responseData = response.body().string();
-                    }
-
-                    if (responseData.contains(username) && responseData.contains(password)) {
-                        runOnUiThread(() -> Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show());
-                    } else {
-                        runOnUiThread(() -> Toast.makeText(this, "Wrong credentials", Toast.LENGTH_SHORT).show());
-                    }
+                    runOnUiThread(() -> Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show());
+                } else if (response.code() == 401) {
+                    runOnUiThread(() -> Toast.makeText(this, "Wrong credentials", Toast.LENGTH_SHORT).show());
                 } else {
-                    runOnUiThread(() -> Toast.makeText(this, "Request failed", Toast.LENGTH_SHORT).show());
+                    runOnUiThread(() -> Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show());
                 }
 
                 response.close();
 
             } catch (IOException e) {
                 e.printStackTrace();
-                runOnUiThread(() -> Toast.makeText(this, "Connexion problem", Toast.LENGTH_SHORT).show());
+                runOnUiThread(() -> Toast.makeText(this, "Connection problem", Toast.LENGTH_SHORT).show());
             }
         }).start();
     }
@@ -87,28 +92,33 @@ public class LoginActivity extends AppCompatActivity {
             try {
                 OkHttpClient client = new OkHttpClient();
 
+                // this is the json we send to the api
                 String json = "{\"username\": \"" + username + "\", \"password\": \"" + password + "\"}";
-
                 RequestBody body = RequestBody.create(json, MediaType.parse("application/json"));
 
+                // creating the request
                 Request request = new Request.Builder()
-                        .url(BASE_URL + "/register")  // La route pour enregistrer l'utilisateur
-                        .post(body)  // Méthode POST pour l'enregistrement
+                        .url(BASE_URL + "/register")
+                        .post(body)
                         .build();
 
+                // executing the request
                 Response response = client.newCall(request).execute();
 
                 if (response.isSuccessful()) {
                     runOnUiThread(() -> Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show());
+                } else if (response.code() == 400) {
+                    runOnUiThread(() -> Toast.makeText(this, "User already exists", Toast.LENGTH_SHORT).show());
                 } else {
                     runOnUiThread(() -> Toast.makeText(this, "Registration failed", Toast.LENGTH_SHORT).show());
                 }
+
+                response.close();
+
             } catch (IOException e) {
                 e.printStackTrace();
                 runOnUiThread(() -> Toast.makeText(this, "Registration problem", Toast.LENGTH_SHORT).show());
             }
         }).start();
     }
-
 }
-
